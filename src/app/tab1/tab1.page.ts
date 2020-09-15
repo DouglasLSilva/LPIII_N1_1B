80e6bd48c633d3f262b125326be352a39f2450a5
Douglas Lopes <douglas_lopes.s@hotmail.com>
2020-09-15 20:34:41 -0300

    First API - Chuck Norris

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+export interface responseApi {
+  type: string;
+  value: Joke;
+}
+
+export interface Joke {
+  joke: string;
+}
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
 
-  constructor() {}
+  piada: string
+  constructor(private http : HttpClient) {}
 
+  BuscarNovaPiada(){
+    this.http.get<responseApi>('http://api.icndb.com/jokes/random').subscribe(data => {
+      this.piada = data.value.joke;
+    },
+    error => {
+      alert("Ocorreu um erro ao consultar a API")
+    });
+  }
 }
+
+
